961c45235e567996dbe17ca401c24dc1ca3aa9f7
Hemsas Riane <rayan.hemsas@gmail.com>
2023-07-30 20:33:07 +0200

    Completed assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+function receivesAFunction(callBackFunction){
+
+ callBackFunction();
+}
+namedFunction();
+function  returnsANamedFunction (){
+
+    return function namedFunction()
+    {
+
+    };
+    }
+    function  returnsAnAnonymousFunction(){
+        return function(){
+            
+        };
+    }
+   
